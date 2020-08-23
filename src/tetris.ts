ef59ba302bf6c256210ae655410b7bb93279a995
kubotak0630 <kubotak@f01.itscom.net>
2020-08-23 12:32:37 +0900

    Adjust vibration again

--- src/tetris.ts
+++ src/tetris.ts
@@ -186,7 +186,7 @@ export default class Tetris {
           //ライン消去処理(消すだけ)
           const delLineNum = this.board.deleteLineStep1(this._mino.getMinoLines());
           if (delLineNum) {
-            window.navigator.vibrate(120);
+            window.navigator.vibrate(80);
             this._nowLineDeleteFrameCnt = 20;
             this._scoreCtrl.addLineCnt(delLineNum);
           } else {
